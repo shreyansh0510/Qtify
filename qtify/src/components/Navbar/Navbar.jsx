842de63399a1c51a1cf1784ad085f747299e2d36
shreyansh <shreyanshsingh0510@gmail.com>
2024-02-21 15:23:44 +0530

    basic navbar

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import logoImage from "../../assets/logo.png";
+import SearchIcon from "../../assets/search.jpg";
+
+
 
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
@@ -9,9 +13,27 @@ function Navbar() {
   return (
     <>
       <nav className={styles.navbar}>
-        <Logo />
-        <Search placeholder="Search a song of your choice" />
-        <Button children="Give Feedback" />
+        <div>
+          <img src={logoImage} alt="logo" width={67} />
+        </div>
+        <div className="wrapper">
+          <input
+            type="text"
+            placeholder={"Search a song of your choice"}
+            // name={search}
+            // value={search}
+            // onChange={handleChange}
+            className={styles.search}
+          />
+          <button className={styles.searchButton} type="submit">
+            <img src={SearchIcon} alt="search-icon" />
+          </button>
+        </div>
+        <div className={styles.button}>
+          <button>
+            Give Feedback
+          </button>
+        </div>
       </nav>
     </>
   );
