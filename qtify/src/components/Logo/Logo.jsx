0c6055caa496ad0776db96957690a0b85a8e1b1c
shreyansh <shreyanshsingh0510@gmail.com>
2024-02-21 16:38:30 +0530

    UPDATED LOGO

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import logoImage from "../../assets/logo.png";
+import styles from "./Logo.module.css"
 
 function Logo() {
-  return <img src={logoImage} alt="logo" width={67} />;
+  return (
+    <>
+      <div className={styles.logoContainer} >
+        <img src={logoImage} alt="logo" width={67} />
+      </div>
+    </>
+  )
 }
 
 export default Logo;
